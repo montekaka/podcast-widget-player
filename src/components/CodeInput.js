68c6121e4330bf62782434d9391440b46326b004
montekaka <josh.chen.jia@gmail.com>
2021-08-13 23:48:30 -0700

    adds copy widget code

--- src/components/CodeInput.js
+++ src/components/CodeInput.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useState} from "react";
 import {useAtom} from 'jotai'
 import {
   rssFeedAtom,
@@ -9,6 +9,7 @@ import './input.css'
 const CodeInput = () => {
 
   const [code] = useAtom(getWidgetCodeAtom)
+  const [copyButtonLabel, setCopyButtonLabel] = useState('Copy')
 
   const handleChange = (event) => {
     
@@ -16,15 +17,30 @@ const CodeInput = () => {
 
   const handleClick = () => {
     // setRssFeed(value);
+    const copyText = document.querySelector(`#input-widget-code`);
+    copyText.select();
+    document.execCommand("Copy");
+    setCopyButtonLabel('Copied');
+    // change back to Copy in 3 seconds
+    setTimeout(() => {
+      setCopyButtonLabel("Copy");
+    }, 3000)        
   }
 
   return (
     <div className="basic-input">
       <div className="css-79elbks">
-        <input type="search" aria-label="Rss feed" placeholder="Enter your RSS feed" value={code} className="css-1j9w4jl simple-input" onChange={handleChange}/>        
+        <input 
+          type="search" 
+          aria-label="Rss feed" 
+          placeholder="Enter your RSS feed" 
+          value={code} 
+          className="css-1j9w4jl simple-input"
+          id="input-widget-code"
+          onChange={handleChange}/>
       </div>  
       <div className="button-container">
-        <button className="css-s700uk" onClick={handleClick}>Copy</button>
+        <button className="css-s700uk" onClick={handleClick}>{copyButtonLabel}</button>
       </div>    
     </div>  
   )
--- src/components/Input.js
+++ src/components/Input.js
@@ -22,9 +22,9 @@ const Input = () => {
     <div className="css-68bi76">
       <div className="css-79elbk">
         <div className="css-1jo5a5k">
-          <svg viewBox="0 0 24 24" width="24" height="24" stroke="currentColor" stroke-width="2" fill="none" stroke-linecap="round" stroke-linejoin="round" class="css-1hy7qas"><circle cx="11" cy="11" r="8"></circle><line x1="21" y1="21" x2="16.65" y2="16.65"></line></svg>
+          <svg viewBox="0 0 24 24" width="24" height="24" stroke="currentColor" strokeWidth="2" fill="none" strokeLinecap="round" strokeLinejoin="round" className="css-1hy7qas"><circle cx="11" cy="11" r="8"></circle><line x1="21" y1="21" x2="16.65" y2="16.65"></line></svg>
         </div>
-        <input type="search" aria-label="Rss feed" placeholder="Enter your RSS feed" value={value} className="css-1j9w4jl" onChange={handleChange}/>        
+        <input type="search" aria-label="Rss feed" placeholder="Enter your RSS feed" value={value} className="css-1j9w4jl" onChange={handleChange}/>
       </div>  
       <div className="button-container">
         <button className="css-s700uk" onClick={handleClick}>Search</button>
